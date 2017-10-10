4417f9c16b5df3578584b721921ae250540c1291
Sean O Hora <D00186551@student.dkit.ie>
2017-10-10 20:44:47 +0100

    Need to work on base of locale.

--- InternationalEX1/src/internationalex1/Exercise1.java
+++ InternationalEX1/src/internationalex1/Exercise1.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 package internationalex1;
-
-import java.util.Arrays;
 import java.util.Locale;
 
 /**
--- InternationalEX1/src/internationalex1/InternationalEX1.java
+++ InternationalEX1/src/internationalex1/InternationalEX1.java
@@ -5,6 +5,10 @@
  */
 package internationalex1;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 /**
  *
  * @author Seanoh
@@ -16,7 +20,18 @@ public class InternationalEX1 {
      */
     public static void main(String[] args) {
         Exercise1 work = new Exercise1();
-        work.displayAllLocale();
+        //work.displayAllLocale();
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a language code:");
+        String language = input.nextLine();
+
+        Locale currentLocale = new Locale(language);
+        ResourceBundle messages = ResourceBundle.getBundle("text", currentLocale);
+
+        System.out.println(messages.getString("login.label.username"));
+        System.out.println(messages.getString("login.label.password"));
+        System.out.println(messages.getString("login.button.submit"));
     }
     
 }
